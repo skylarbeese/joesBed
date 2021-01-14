369d9ba12e760699c9a825ed37567aa37b3805dc
skylarbeese <skylarbeese@gmail.com>
2021-01-14 16:00:58 -0700

    fgfgdf

---
+++ main.js
@@ -0,0 +1,12 @@
+let header = document.querySelector('.comp') 
+header.innerHTML = "Joes Bed and breakfist"
+
+let header1 = document.querySelector('.header2') 
+header1.innerHTML = "Joes Bed and breakfist the best place in utah"
+
+let userName = prompt('what is you name?')
+
+let message = 'Hello ' + userName + ' welcome to the best stay in salt lake city, Utah'
+
+let mess = document.querySelector('.mess')
+mess.innerHTML = message
